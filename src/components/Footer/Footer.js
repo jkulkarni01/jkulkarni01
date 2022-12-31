0f32fe9162cccfa90c42ce9237870894fcf7d7b1
gkulk007 <gkulk007@gmail.com>
2022-12-31 15:11:54 +0530

    Final Version

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import './Footer.css'
 const Footer = () => (
   <footer className='footer'>
     <a
-      href='https://github.com/jkulkarni'
+      href='https://github.com/jkulkarni01'
       className='link footer__link'
     >
       Made with ❤️ by Jui Kulkarni
